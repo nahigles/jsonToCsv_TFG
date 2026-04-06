d7ad9f3ae13873b4dba3224beb880d7e882e3ecf
Nahia <82345386+nahigles@users.noreply.github.com>
2026-04-06 12:28:12 +0200

    Update jsonToCsv.py

--- jsonToCsv.py
+++ jsonToCsv.py
@@ -1,104 +1,88 @@
 import csv
 import json
 
-tiempoSolicitudParada = ''
-tiempoSolicitudDormir = ''
+tipoPregunta = '-'
+preguntaCorrecta = '-'
+tiempoSolicitudParada = '-'
+tiempoSolicitudDormir = '-'
 timeStamp = ''
 
 # Si tiempoSolicitudParada o tiempoSolicitudDormir es  - es que no hay pregunta
 
-def MCEvent(i, e): 
+def MCEvent(i, e, info): 
 
+    # tipoPregunta = info[0]
+    # preguntaCorrecta = info[1]
+    # tiempoSolicitudParada = info[2]
+    # tiempoSolicitudDormir = info[3]
     tipoPregunta = '-'
     preguntaCorrecta = '-'
+    tiempoSolicitudParada = '-'
+    tiempoSolicitudDormir = '-'
 
     if e == "Paciente y numero de sesion":
-        print(f"{e} is a weekend.")
+        a = ''
 
     elif e == "Iniciando juego":
-        print(f"{e} is a weekend.")
+        a = ''
 
     elif e == "Empieza pregunta y decision":
-        print(f"{e} is a weekend.")
+        a = ''
     elif e == "Termina pregunta y decision":
-        print(f"{e} is a weekday.")
+        a = ''
     elif e == "Planificacion guardada":
-        print(f"{e} is a weekday.")
+        f = ''
 
     elif e == "Hora de salda seleccionada":
-        print(f"{e} is a weekday.")
+        e = ''
 
     elif e == "Solicitud de parada":
         tipoPregunta = 'Parada'
         tiempoSolicitudParada = timeStamp
-        print(f"{e} is a weekday.")
 
     elif e == "Respuesta de parada SI":
-        print(f"{e} is a weekday.")
+         tipoPregunta = 'Parada'
 
     elif e == "Respuesta de parada NO":
-        print(f"{e} is a weekday.")
+         tipoPregunta = 'Parada'
 
     elif e == "Solicitud de dormir":
         tipoPregunta = 'Dormir'
         tiempoSolicitudDormir = timeStamp
-        print(f"{e} is a weekday.")
 
     elif e == "Respuesta de dormir SI":
+        tipoPregunta = 'Dormir'
         preguntaCorrecta = 'Correcto'
-        print(f"{e} is a weekday.")  
 
     elif e == "Respuesta de dormir NO":
+        tipoPregunta = 'Dormir'
         preguntaCorrecta = 'Erroneo'
-        print(f"{e} is a weekday.")  
 
     elif e == "Inicio de dormir":
-        print(f"{e} is a weekday.")
-
+        c = ''
     elif e == "Ubcacion enviada":
         tipoPregunta = 'Ubicacion'
-        print(f"{e} is a weekday.")
 
     elif e == "Ubicacion bien enviada":
+        tipoPregunta = 'Ubicacion'
         preguntaCorrecta = 'Correcto'
-        print(f"{e} is a weekday.")
 
     elif e == "Envio de ubicación omitido":
+        tipoPregunta = 'Ubicacion'
         preguntaCorrecta = 'Erroneo'
-        print(f"{e} is a weekday.")
 
     elif e == "Tiempo de respuesta excedido":
         preguntaCorrecta = 'Erroneo'
-        print(f"{e} is a weekday.")
 
     elif e == "Se cumplen las reglas":
-        print(f"{e} is a weekday.")
+        a = ''
     elif e == "Se incumplen las reglas":
-        print(f"{e} is a weekday.")
+        b = ''
 
-    return tipoPregunta, preguntaCorrecta
-
-    # EventosInfo.MCEmpiezaDecision, "Empieza pregunta y decision" },
-    #     { EventosInfo.MCTerminaDecision, "Termina pregunta y decision" },
-    #     { EventosInfo.MCPlanifcacionGuardada, "Planificacion guardada" },
-    #     { EventosInfo.MCHoraSalidaSeleccionada, "Hora de salda seleccionada" },
-    #     { EventosInfo.MCPreguntaParada, "Solicitud de parada" },
-    #     { EventosInfo.MCRespuestaParadaSi, "Respuesta de parada SI" },
-    #     { EventosInfo.MCRespuestaParadaNo, "Respuesta de parada NO" },
-    #     { EventosInfo.MCPreguntaDormir, "Solicitud de dormir" },
-    #     { EventosInfo.MCRespuestaDormirSi, "Respuesta de dormir SI" },
-    #     { EventosInfo.MCRespuestaDormirNo, "Respuesta de dormir NO" },
-    #     { EventosInfo.MCComienzoDormir, "Inicio de dormir" },
-    #     { EventosInfo.MCTerminoDormir, "Final de dormir" },
-    #     { EventosInfo.MCUbicacionEnviada, "Ubcacion enviada" },
-    #     { EventosInfo.MCUbicacionEnviadaBien, "Ubicacion bien enviada" },
-    #     { EventosInfo.MCUbicacionOmisionEnvio, "Envio de ubicación omitido" },
-    #     { EventosInfo.MCPreguntaSinRespuesta, "Tiempo de respuesta excedido" },
-    #     { EventosInfo.MCReglasCumplidas, "Se cumplen las reglas" },
-    #     { EventosInfo.MCReglasIncumplidas, "Se incumplen las reglas" },
+    return tipoPregunta, preguntaCorrecta, tiempoSolicitudParada, tiempoSolicitudDormir
 
 
-ruta =  "Mision Colombia/"
+ruta =  "../Mision Colombia/"
 name = "2_MisionColombia_2025-10-30_1.json"
 
 with open(ruta + name) as archivo:
@@ -142,26 +126,33 @@ for i in range(1,numEvents):
     timeStamp = splitT[1]
 
     eventName = list(datos_JSON[name][i]["Eventos"][0].keys())[0]
-    infoEvent = MCEvent(i,eventName)
+    infoEvent = MCEvent(i,eventName, [tipoPregunta, preguntaCorrecta, tiempoSolicitudParada, tiempoSolicitudDormir])
 
+    tipoPregunta = infoEvent[0]
+    preguntaCorrecta = infoEvent[1]
+    tiempoSolicitudParada = infoEvent[2]
+    tiempoSolicitudDormir = infoEvent[3]
 
-    # Siguiente fila que quiero anyadir
-    dataFil = [splitText[0][-1], splitText[1][-1],1,  timeStamp, 9.0,'-', infoEvent[0], infoEvent[1]]
-    data.append(dataFil) # Anyado al final de la lista de datos
-    print(f"Iteración {i}")
+# Escribo si ell tiempoubi no esta en - y es ubi o so tiempo dormir no esta en - y es dormir (negando todo eso)
+# significa que no esta pendiente de dale a responder
+    if not((tiempoSolicitudDormir != '-' and tipoPregunta == 'Dormir') or (tiempoSolicitudParada  != '-' and tipoPregunta == 'Parada')):
+       
+       # Si es Dormir 
+        if tipoPregunta == 'Dormir':
+            dataFil = [splitText[0][-1], splitText[1][-1],1,  tiempoSolicitudDormir, timeStamp,'-', tipoPregunta, preguntaCorrecta]
+       # Si es Parada
 
+       # Si es Ubicacion lo pongo sinmas
+        # Siguiente fila que quiero anyadir
+        else:
+            dataFil = [splitText[0][-1], splitText[1][-1],1,  timeStamp, 9.0,'-', tipoPregunta, preguntaCorrecta]
+        data.append(dataFil) # Anyado al final de la lista de datos
+        print(f"Tiempo Solicitud dormir: {tiempoSolicitudDormir} y Tiempo sol parada: {tiempoSolicitudParada} ,Tipo: {infoEvent[0]}")
+        print(f"Iteración {i}")
 
-
-# data = [['ID','Sesion', 'Tiempo_Pregunta', 'Tiempo_Respuesta','Respuesta'],
-#      [slitText[0][-1], slitText[1][-1], 2, 9.0, 'Correcta'],
-#      ['Sanchit', 'COE', 2, 9.1, 'Correcta'],
-#      ['Aditya', 'IT', 2, 9.3, 'Correcta'],
-#      ['Sagar', 'SE', 1, 9.5, 'Correcta'],
-#      ['Prateek', 'MCE', 3, 7.8, 'Correcta'],
-#      ['Sahil', 'EP', 2, 9.1, 'Correcta']]
 
 # Abro archivo .csv para guardar los datos leidos
-file =  open('datos.csv', 'w', newline='')
+file =  open('../datos.csv', 'w', newline='')
 writer = csv.writer(file)
 writer.writerows(data)
 file.close()
