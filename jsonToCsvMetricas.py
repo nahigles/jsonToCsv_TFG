6423d911d41d44d37dfb96eae6082c48b6f3b7b8
Nahia <82345386+nahigles@users.noreply.github.com>
2026-07-28 14:05:57 +0200

    Medias porcentaje, correlaciones
    Medias de porcentaje de acierto y fallo en lo planificado y correlaciones. Empiezo a calcular medias con pasos planificados > 0

--- jsonToCsvMetricas.py
+++ jsonToCsvMetricas.py
@@ -570,8 +570,8 @@ for name in nombresArchivos:
        
 
 # Escribo datos finales
-writeDatas = True
-writeCorrelacion = False
+writeDatas = False
+writeCorrelacion = True
 writeType = 0b1111
 writeDatosCrudos = False
 
@@ -591,21 +591,21 @@ if writeType & 0b0100:
     if writeDatas:
         saveCsvExcel('datosIntentos', dataIntentos)
     if writeCorrelacion:
-        corr('datosIntentos',2, False)
-        #corr('datosIntentos',2, True)
+        #corr('datosIntentos',2, False)
+        corr('datosIntentos',2, True)
 
 # DATOS POR NIVEL 3
 if writeType & 0b0010:
     if writeDatas:
         saveCsvExcel('datosNiveles', dataNiveles)
     if writeCorrelacion:
-        corr('datosNiveles', 3, False)
-        #corr('datosNiveles', 3, True)
+        #corr('datosNiveles', 3, False)
+        corr('datosNiveles', 3, True)
 
 # DATOS POR SESION 4
 if writeType & 0b0001:
     if writeDatas:
         saveCsvExcel('datosSesiones', dataSesiones)
     if writeCorrelacion:
-        corr('datosSesiones', 4, False)
-        #corr('datosSesiones', 4, True)
+        #corr('datosSesiones', 4, False)
+        corr('datosSesiones', 4, True)
